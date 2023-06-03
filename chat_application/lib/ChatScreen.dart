ea1dbc9c417ece28230b56b21921205e7e6c8885
Rutesh R Zalavadiya <zalavadiyarutesh@gmail.com>
2023-06-03 22:30:26 +0530

    First commit

---
+++ chat_application/lib/ChatScreen.dart
@@ -0,0 +1,78 @@
+import 'package:chat_application/ChatMesage.dart';
+import 'package:flutter/material.dart';
+import 'ChatMesage.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = new TextEditingController();
+  final List<ChatMessage> _messages = <ChatMessage>[];
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+    ChatMessage message = new ChatMessage(
+      text: text,
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
+  }
+
+  Widget _textComposerWidget() {
+    return new IconTheme(
+      data: new IconThemeData(color: Colors.blue),
+      child: new Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: new Row(
+          children: [
+            new Flexible(
+              child: new TextField(
+                decoration:
+                    new InputDecoration.collapsed(hintText: "Send A Message"),
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: new IconButton(
+                icon: new Icon(Icons.send),
+                onPressed: () => _handleSubmitted(_textController.text),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Column(
+      children: [
+        new Flexible(
+          child: new ListView.builder(
+            padding: new EdgeInsets.all(8.0),
+            reverse: true,
+            itemBuilder: (_, int _index) => _messages[_index],
+            itemCount: _messages.length,
+          ),
+        ),
+        new Divider(
+          height: 1.0,
+        ),
+        new Container(
+          decoration: new BoxDecoration(
+            color: Theme.of(context).cardColor,
+          ),
+          child: _textComposerWidget(),
+        )
+      ],
+    );
+  }
+}
